4433b1023a33df3fd9968b15178f4ec99914c5ee
alimraanDawood <alimraandawoodgulam@gmail.com>
2025-01-26 23:26:15 +0300

    Fixing authentication issues

--- pages/auth/signup/index.vue
+++ pages/auth/signup/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="flex flex-col h-[100dvh] w-screen overflow-y-scroll overflow-x-scroll lg:p-5 items-center justify-center  bg-black/10">
+    <div
+        class="flex flex-col h-[100dvh] w-screen overflow-y-scroll overflow-x-scroll lg:p-5 items-center justify-center  bg-black/10">
         <div class="bg-white md:border md:shadow h-full md:h-fit md:rounded-lg w-full max-w-[500px] divide-y">
             <div class="flex flex-row p-3 items-center justify-between rounded-t-xl border bg-[#fafafa]">
                 <button @click="$router.go(-1)" class="text-sm font-medium text-primary">Cancel</button>
@@ -17,12 +18,16 @@
 
             <div class="flex flex-col">
                 <div class="flex flex-row w-full p-3 items-center gap-3">
-                    <div class="h-12 aspect-square border bg-center bg-cover rounded-full grid place-items-center" :style="{ backgroundImage: `url('${ createObjectUrl(this.signUp.avatar) }')` }" >
-                        <span class="font-medium text-black/50 text-lg" v-if="signUp.avatar === null">{{  signUp.name[0]  }}</span>
+                    <div class="h-12 aspect-square border bg-center bg-cover rounded-full grid place-items-center"
+                        :style="{ backgroundImage: `url('${createObjectUrl(this.signUp.avatar)}')` }">
+                        <span class="font-medium text-black/50 text-lg" v-if="signUp.avatar === null">{{ signUp.name[0]
+                            }}</span>
                     </div>
                     <div class="flex flex-row gap-1 items-center">
-                        <button :disabled="signUp.avatar !== null" @click="addImage" class="text-sm font-medium px-4 p-1 border rounded disabled:opacity-50">Upload</button>
-                        <button :disabled="signUp.avatar === null" @click="signUp.avatar = null" class="text-sm font-medium px-4 p-1 border rounded disabled:opacity-50">Remove</button>
+                        <button :disabled="signUp.avatar !== null" @click="addImage"
+                            class="text-sm font-medium px-4 p-1 border rounded disabled:opacity-50">Upload</button>
+                        <button :disabled="signUp.avatar === null" @click="signUp.avatar = null"
+                            class="text-sm font-medium px-4 p-1 border rounded disabled:opacity-50">Remove</button>
                     </div>
                 </div>
             </div>
@@ -40,7 +45,7 @@
                 <div class="flex flex-col gap-1 p-3">
                     <span class="font-medium text-sm">Email address</span>
                     <input class="p-2 border w-full rounded-lg" v-model="v$.signUp.email.$model"
-                        placeholder="john.doe@gmail.com" />
+                        placeholder="john.doe@gmail.com" type="email" />
                     <div class="input-errors" v-for="error of v$.signUp.email.$errors" :key="error.$uid">
                         <div class="text-red-500 text-xs">{{ error.$message }}</div>
                     </div>
@@ -51,7 +56,7 @@
                     <PinInput class="text-sm" id="pin-input" v-model="v$.signUp.phone.$model" placeholder="○"
                         @complete="handleComplete">
                         <PinInputGroup>
-                            <PinInputInput  class="p-0 w-[10%]" v-for="(id, index) in 10" :key="id" :index="index" />
+                            <PinInputInput class="p-0 w-[10%]" v-for="(id, index) in 10" :key="id" :index="index" />
                         </PinInputGroup>
                     </PinInput>
                     <div class="input-errors" v-for="error of v$.signUp.phone.$errors" :key="error.$uid">
@@ -64,7 +69,17 @@
             <div class="flex flex-col">
                 <div class="flex flex-col p-3">
                     <span class="font-medium text-sm">Password</span>
-                    <input v-model="v$.signUp.password.$model" class="p-2 border w-full rounded-lg" />
+                    <div class="border-2 flex flex-row gap-2 rounded-lg focus-within:border-primary">
+
+                        <input :type="signUp.passwordVisible ? 'text' : 'password'"
+                            class="p-2 focus:outline-none w-full rounded-lg" v-model="v$.signUp.password.$model"
+                            placeholder="***********" />
+
+                        <button class="p-2" @click="signUp.passwordVisible = !signUp.passwordVisible">
+                            <PhosphorIconEye v-if="signUp.passwordVisible" :size="20" />
+                            <PhosphorIconEyeSlash v-else :size="20" />
+                        </button>
+                    </div>
                     <div class="input-errors" v-for="error of v$.signUp.password.$errors" :key="error.$uid">
                         <div class="text-red-500 text-xs">{{ error.$message }}</div>
                     </div>
@@ -72,7 +87,17 @@
 
                 <div class="flex flex-col p-3">
                     <span class="font-medium text-sm">Confirm Password</span>
-                    <input v-model="v$.signUp.passwordConfirm.$model" class="p-2 border w-full rounded-lg" />
+                    <div class="border-2 flex flex-row gap-2 rounded-lg focus-within:border-primary">
+
+                        <input :type="signUp.passwordConfirmVisible ? 'text' : 'password'"
+                            class="p-2 focus:outline-none w-full rounded-lg" v-model="v$.signUp.passwordConfirm.$model"
+                            placeholder="***********" />
+
+                        <button class="p-2" @click="signUp.passwordConfirmVisible = !signUp.passwordConfirmVisible">
+                            <PhosphorIconEye v-if="signUp.passwordConfirmVisible" :size="20" />
+                            <PhosphorIconEyeSlash v-else :size="20" />
+                        </button>
+                    </div>
                     <div class="input-errors" v-for="error of v$.signUp.passwordConfirm.$errors" :key="error.$uid">
                         <div class="text-red-500 text-xs">{{ error.$message }}</div>
                     </div>
@@ -80,12 +105,11 @@
             </div>
 
             <div class="flex flex-col w-full p-3">
-                <button @click="signUpAdmin" 
-                        :disabled="signUp.loading"
-                        class="disabled:opacity-50 bg-primary font-medium text-white whitespace-nowrap flex flex-row items-center justify-center px-4 p-1 rounded shadow w-full">
-                        <PhosphorIconSpinner v-if="signUp.loading" :size="20" class="animate-spin" />
-                        <span v-else>Submit Account Request</span>
-                    </button>
+                <button @click="signUpAdmin" :disabled="signUp.loading"
+                    class="disabled:opacity-50 bg-primary font-medium text-white whitespace-nowrap flex flex-row items-center justify-center px-4 p-1 rounded shadow w-full">
+                    <PhosphorIconSpinner v-if="signUp.loading" :size="20" class="animate-spin" />
+                    <span v-else>Submit Account Request</span>
+                </button>
             </div>
         </div>
     </div>
@@ -117,7 +141,9 @@ export default {
                 email: '',
                 phone: '07'.split(''),
                 password: '',
-                passwordConfirm: ''
+                passwordConfirm: '',
+                passwordVisible: false,
+                passwordConfirmVisible: false,
             }
         }
     },
@@ -147,7 +173,7 @@ export default {
             this.signUp.passwordConfirm = '';
         },
         createObjectUrl(object) {
-            if(object) {
+            if (object) {
                 return URL.createObjectURL(object);
             }
         },
@@ -166,16 +192,16 @@ export default {
         },
         async signUpAdmin() {
             this.v$.$validate();
-            if(this.v$.$errors.length === 0) {
+            if (this.v$.$errors.length === 0) {
                 try {
                     this.signUp.loading = true;
-                    const user = await signUp({...this.signUp, phone: this.signUp.phone.join('')});
+                    const user = await signUp({ ...this.signUp, phone: this.signUp.phone.join('') });
 
-                    if(user) {
+                    if (user) {
                         toast('Account Request Submitted!');
                         this.$router.push('/auth/verification_pending/')
                     }
-                } catch(e) {
+                } catch (e) {
                     console.error(e);
                     toast.error('Failed to create account request! Please try again!')
                 }
