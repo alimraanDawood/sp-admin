1c9845a4bf0575025045aee26ad7e2a1a7551053
alimraanDawood <alimraandawoodgulam@gmail.com>
2025-02-07 19:01:38 +0300

    Added Cost and fixed a few bugs

--- services/utils/index.ts
+++ services/utils/index.ts
@@ -1,6 +1,6 @@
 import Pocketbase from 'pocketbase';
 
-const pb = new Pocketbase("http://localhost:8090");
+const pb = new Pocketbase("https://sports-palace.pockethost.io");
 
 
 export function getFileUrl(record : any, url : string, options : any) {
